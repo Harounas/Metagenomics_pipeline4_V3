1ebcdda08079b4d8066fa348072fb41ed1d65309
Harouna Soumare <140737983+Harounas@users.noreply.github.com>
2025-06-03 13:01:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Metagenomics_pipeline4_V2",
+    name="Metagenomics_pipeline4_V3",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -11,7 +11,7 @@ setup(
     entry_points={
     "console_scripts": [
         
-"run_metagenomics_pl1=Metagenomics_pipeline4_V2.scripts.run_metagenomics_pl1:main",
+"run_metagenomics_pl2=Metagenomics_pipeline4_V3.scripts.run_metagenomics_pl2:main",
     ],
 },
 
@@ -20,11 +20,11 @@ setup(
     description="A bioinformatics pipeline for trimming, host depletion, and taxonomic classification",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Harounas/Metagenomics_pipeline4_V2.git",
+    url="https://github.com/Harounas/Metagenomics_pipeline4_V3.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9',
 )
